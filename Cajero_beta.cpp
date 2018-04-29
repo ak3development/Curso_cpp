6d499aa034c9aef86a63ab5d87b3e2020cd6bad3
ak3 <ak3tres.development@protonmail.com>
2018-04-28 23:30:32 -0300

    Beta de un cajero virtual basico

---
+++ Cajero_beta.cpp
@@ -0,0 +1,45 @@
+/*Beta de un cajero virtual en donde el ususario empieza con 1000 dolares*/
+
+#include <iostream>
+
+int main(){
+
+	int saldo = 1000, retiro = 0, opc, deposito;
+
+	std::cout<<"\tBienvenido al cajero virtual."<<std::endl;
+	std::cout<<"\tElija la opcion que decea realizar."<<std::endl;
+	std::cout<<"\t1.Ingreso de dinero."<<std::endl;
+	std::cout<<"\t2.Retiro de dinero."<<std::endl;
+	std::cout<<"\t3.Salir"<<std::endl;
+	std::cin>>opc;
+
+	switch(opc){
+
+		case 1:
+			std::cout<<"Ingrese el monto a depositar. << ";
+			std::cin>>deposito;
+			saldo += deposito;
+			std::cout<<"tu nuevo saldo es "<< saldo <<std::endl;
+			break;
+		case 2:
+			std::cout<<"Ingrese el monto a retirar. << ";
+			std::cin>>retiro;
+
+			if(retiro > saldo)
+				std::cout<<"No cuentas con ese saldo."<<std::endl;
+
+			else
+				std::cout<<"Retirado "<< retiro <<" desde tu cuenta"<<std::endl;
+				saldo -= retiro;
+				std::cout<<"Tu nuevo saldo es de "<< saldo <<std::endl;
+			break;
+
+		case 3:
+			break;
+
+	}
+
+
+return 0;
+
+}
