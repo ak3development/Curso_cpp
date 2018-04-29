3cfb7eb66f920c86f3c87f3ef8f20dff567d840c
ak3 <ak3tres.development@protonmail.com>
2018-04-28 21:28:07 -0300

    Pide al usuario un numero y devuelve el nombre del mes si esta en el rango

---
+++ Meses.cpp
@@ -0,0 +1,35 @@
+/* Mostrar los mese del año, pidiendole al ususario un numero ente (1-12)
+y mostrar el mes al que corresponde*/
+
+#include <iostream>
+
+int main(){
+
+	int mes;
+
+	std::cout<<"Ingrese un numero para mostrarle el mes. \n<<";
+	std::cin>>mes;
+
+	switch(mes){
+		case 1:std::cout<<"Enero"<<std::endl;break;
+		case 2:std::cout<<"Febrero"<<std::endl;break;
+		case 3:std::cout<<"Marzo"<<std::endl;break;
+		case 4:std::cout<<"Abril"<<std::endl;break;
+		case 5:std::cout<<"Mayo"<<std::endl;break;
+		case 6:std::cout<<"Junio"<<std::endl;break;
+		case 7:std::cout<<"Julio"<<std::endl;break;
+		case 8:std::cout<<"Agosto"<<std::endl;break;
+		case 9:std::cout<<"Septiembre"<<std::endl;break;
+		case 10:std::cout<<"Octubre"<<std::endl;break;
+		case 11:std::cout<<"Noviembre"<<std::endl;break;
+		case 12:std::cout<<"Diciembre"<<std::endl;break;
+		default:std::cout<<"No hay un mes fuera de ese rango."<<std::endl;
+	}
+
+
+
+
+
+
+return 0;
+}
